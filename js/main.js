bad54c2a4a13927f93e428ae41a950d621d5c664
Doaa Othman <doaaothman18@gmail.com>
2020-04-09 01:23:58 +0200

    last Update

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
-var collapsedBtn =document.getElementsByClassName('card');
-var collapsedchild =document.getElementsByClassName('card-header');
-
-
-console.log('dooo'+collapsedBtn.length);
-for(let i=0;i<collapsedBtn.length;i++){
-    collapsedBtn[i].addEventListener('click',callapse);
-    function callapse(){
-        var buttoncolor =collapsedBtn[i].querySelector('button');
+var collapsedBtn = document.getElementsByClassName('card');
+var collapsedchild = document.getElementsByClassName('card-header');
+console.log('dooo' + collapsedBtn.length);
+for (let i = 0; i < collapsedBtn.length; i++) {
+    collapsedBtn[i].addEventListener('click', callapse);
+    function callapse() {
+        var buttoncolor = collapsedBtn[i].querySelector('button');
         collapsedBtn[i].firstElementChild.classList.toggle('skyBlueBg')
         buttoncolor.classList.toggle('whitecolor');
-        
-
     }
-    
 }
 // Wrap every letter in a span
-var letter = document.querySelector('.ask button:hover .letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline()
-  .add({
-    targets: letter,
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+var btnHover = document.getElementById('submit');
+btnHover.addEventListener('mouseenter', function () {
+    var letter = btnHover.querySelectorAll('.letter');
+    anime.timeline({}).add({
+        targets: letter,
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+    });
+});
